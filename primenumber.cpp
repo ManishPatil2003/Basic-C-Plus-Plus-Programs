f7ae53e337179a9d8f71317cb008bb35157db1cb
Manish Patil <99351456+ManishPatil2003@users.noreply.github.com>
2022-03-29 19:01:46 +0530

    Add files via upload

---
+++ primenumber.cpp
@@ -0,0 +1,30 @@
+#include <iostream.h>
+#include <conio.h>
+void main()
+{
+	clrscr();
+
+	int n, i, flag = 1;
+
+	cout << "Enter The Number To Check Prime Number Or Not " << endl;
+	cin >> n;
+
+	for (i = 2; i < n; i++)
+	{
+		if (n % i == 0)
+		{
+			flag = 0;
+			break;
+		}
+	}
+
+	if (flag == 1)
+	{
+		cout << n << "  Is Prime Number ";
+	}
+	else
+	{
+		cout << n << "  Is Not Prime Number";
+	}
+	getch();
+}
